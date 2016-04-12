75bd160bd19638b31788f091ced901fea02e6faf
radicarian <mrklevine@gmail.com>
2016-04-12 13:19:15 -0400

    Update dev.sh to reflect correct folder name.

--- BlogJavaDist/src/main/scripts/dev.sh
+++ BlogJavaDist/src/main/scripts/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -Xms1200m -Xmx1300m -Dweb.app.location="../../../BlogJsFrontend/src/main/html/blog" -jar blog.jar
+java -Xms1200m -Xmx1300m -Dweb.app.location="../../../BlogJavaFrontend/src/main/html/blog" -jar blog.jar
 
